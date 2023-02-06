37aec83c2e7c03c9764150f69efecb6fcafb314d
estilbee <odaouphars@gmail.com>
2023-02-06 12:09:44 -0500

    worked out bugs in package.json

---
+++ src/Components/ProjectCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Card(props) {
+    
+    return(
+        <div>
+            project stuff will go here
+        </div>
+    )
+}
+
+
+
+export default Card;
---
+++ src/Components/Projects.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+//not sure if i need useState or useEffect if i'm not using a filter? or do i need a filter?
+
+import Projects from ('./ProjectCard');
+
+
+
+function Projects () {
+
+    const ProjectData = [
+
+        //array of {objects} will be here 
+    ]
+}
